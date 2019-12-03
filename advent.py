95a32e9457cb04c384558884bc95fe618ab740ba
Marzuk Amin <marzuka@student.unimelb.edu.au>
2019-12-03 21:30:48 +1100

    Day 2 done

--- advent.py
+++ advent.py
@@ -10,6 +10,13 @@ total_fuel = 0;
 # Loop through the data and calculate the fuel for each module and add to total
 for mass in modules:
     fuel = math.floor(int(mass)/3) - 2;
+    ext_fuel = math.floor(int(fuel)/3) - 2;
+
+    # Calculate the fuel required for the fuel itself
+    while (ext_fuel >= 0):
+        fuel += ext_fuel;
+        ext_fuel = math.floor(int(ext_fuel)/3) - 2;
+
     # Do not add if the answer is negative
     if (fuel > 0):
         total_fuel += fuel;
